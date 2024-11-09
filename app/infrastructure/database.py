96ca440e9af86173688cf27d6c184bbc26a3ed49
MikaSRahwono <70706750+MikaSRahwono@users.noreply.github.com>
2024-11-09 17:21:10 +0700

    [FIX] Fixing Queue Issues

--- app/infrastructure/database.py
+++ app/infrastructure/database.py
@@ -16,9 +16,9 @@ engine = create_engine(
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def get_db() -> Session:
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# def get_db() -> Session:
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
